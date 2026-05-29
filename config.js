9ef00ff1140811134c62298b50698123a670e2d4
keegou <508785468@qq.com>
2026-05-29 21:17:12 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,13 +1,22 @@
 // ========================================
-// keegou 网站配置文件
-// 改这个文件，所有页面自动更新
+// keegou 网站全局配置
+// 以后只需要改这个文件，所有页面自动更新
 // ========================================
 
-// 头像图片链接（把等号后面的链接替换掉即可）
+// 头像图片链接
 var AVATAR_URL = "https://i.ibb.co/GQKy62D1/1775996572835.png";
 
-// 咖啡罐支付链接（把等号后面的 # 替换成Creem链接）
-var JAR_URL = "#";
+// 咖啡罐支付链接（把 # 替换成Creem链接）
+var JAR_URL = "#";https://www.creem.io/payment/prod_7MN5zP273f4MrhegcYXw6c
+
+// 切片详情页默认购买链接（把 # 替换成Creem链接）
+var BUY_URL = "#";https://www.creem.io/payment/prod_7MN5zP273f4MrhegcYXw6c
+
+// 切片详情页默认下载链接（把 # 替换成PDF链接）
+var DOWNLOAD_URL = "#";
 
 // 邮箱地址
 var EMAIL = "hello@keegou.com";
+
+// 网站域名
+var SITE_URL = "keegou.com";
